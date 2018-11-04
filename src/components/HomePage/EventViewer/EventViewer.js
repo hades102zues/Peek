9ccbf77c53fbd3a5aa0923c47e2d5c51594ca491
Joshua Wiggins <joshua_kar@hotmail.com>
2018-11-04 11:32:55 -0400

    correct the inner workings of the search

--- src/components/HomePage/EventViewer/EventViewer.js
+++ src/components/HomePage/EventViewer/EventViewer.js
@@ -53,12 +53,58 @@ class EventViewer extends Component{
     this.setState({eventDetails}); //update the state
   }
 
+  //determines if an element exists in an array
+   isElementInArray = (ele, currentMatchingTermsArr ) =>{
+      let isElementFound = false;
+
+      currentMatchingTermsArr.forEach( stateArrEle =>{
+          if(ele===stateArrEle)
+            isElementFound = true;
+
+      });
+
+      return isElementFound;
+   }
+
+
+    
+   //this function compares the contents of two array to determine
+   //if they are equal
+
+   isArraysTheSame = (currentMatchingTermsArr) =>{
+
+
+      const stateArr = [...this.state.matchingTerms];
+
+      //this specifies if all terms in the state can be found in the 
+      //list of possible matching terms
+
+      let isAllElementsFound= true;
+
+      if(stateArr.length<1)//not even possible for state to match
+        return false; 
+
+     stateArr.forEach(ele => {
+      //if some element is not found then the arrays are not the same
+          if(!this.isElementInArray(ele, currentMatchingTermsArr)){
+             isAllElementsFound = false;
+          }
+      });
+
+      
+      return isAllElementsFound ;
+   }
+
+
+
+
   //this function determines which events might match the user's input
   //by checking if the entered value is a substring of some event title 
   //and then stores a list of event ids which satify this criteria
 
   matchFinder = () =>{
     const eventDetails = [...this.state.eventDetails];
+
 
     const matchingTerms = eventDetails.reduce((incre, detail)=>{
         
@@ -77,9 +123,24 @@ class EventViewer extends Component{
 
       }, []
     );
-    this.setState({matchingTerms});
+
+   
+    
+    console.log('matchingTerms', matchingTerms);
+    console.log('currentTerms in state', this.state.matchingTerms);
+    console.log('are arrays same:', this.isArraysTheSame(matchingTerms));
+    
+    if(!this.isArraysTheSame(matchingTerms)){
+      console.log('STATE UPDATED');
+       this.setState({matchingTerms});
+     }
+        
 
   }
+
+  //this is necessary to prevent the app from going into an infinite loop
+  //since we updating state within componentDidUpdate
+  //if the matchingTerms are not the same then we should update state
 
 
   componentDidUpdate(){
@@ -94,13 +155,33 @@ class EventViewer extends Component{
     //contains all story ids retrieved from sever
   	const storyIds = [...this.state.storyIds];
 
-    const events = storyIds.map( id =>(
-    	<Event key={id} 
-            storyId={id} 
-            closerClicked = {this.onCloserClickedHandler}
-            getDetails = {this.eventDetailsStorer}
-      />
-    ));
+    const matchingTerms = [...this.state.matchingTerms];
+
+    let events = null;
+
+    //if the matching terms is not empty, in other words the user is searching
+    // then we need to display only those events
+    //that fall in the search criteria. Otherwise simply display all events 
+
+    //console.log('Latest matching terms in state', matchingTerms);
+    if(matchingTerms.length){
+        events = matchingTerms.map( id =>(
+            <Event key={id} 
+                  storyId={id} 
+                  closerClicked = {this.onCloserClickedHandler}
+                  getDetails = {this.eventDetailsStorer}
+            />
+          ));
+    }else{
+          events = storyIds.map( id =>(
+          <Event key={id} 
+                storyId={id} 
+                closerClicked = {this.onCloserClickedHandler}
+                getDetails = {this.eventDetailsStorer}
+          />
+        ));
+
+    }
 
 
   	return (
